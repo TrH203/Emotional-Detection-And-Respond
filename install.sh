e1421ab85330bc8f9108efc5e7c8bed8f8f76978
HoangTrHien_DE <96675314+TrH203@users.noreply.github.com>
2023-08-21 11:24:22 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "Installing Tensorflow library..."
+
+pip install tensorflow
+
+echo "Installing Pandas library..."
+
+pip install pandas
+
+echo "Installing Numpy library..."
+
+pip install numpy
+
